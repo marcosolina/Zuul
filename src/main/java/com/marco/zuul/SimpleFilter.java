81aaf4f0a66a8200c30602b75a36eedbf54d5693
Marco Solina <Marco.Solina@ixico.com>
2021-05-08 18:35:19 +0200

    Improvement

--- src/main/java/com/marco/zuul/SimpleFilter.java
+++ src/main/java/com/marco/zuul/SimpleFilter.java
@@ -2,43 +2,59 @@ package com.marco.zuul;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.http.HttpStatus;
+
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
 public class SimpleFilter extends ZuulFilter {
 
+    @Autowired
+    private RouteLocator routeLocator;
+
     @Override
     public boolean shouldFilter() {
         RequestContext ctx = RequestContext.getCurrentContext();
-
         HttpServletRequest request = ctx.getRequest();
-
         String url = request.getRequestURI();
-
-        if(url.contains("v2")){
-
-            return false;
-
-        }
-        return true;
+        return !url.contains("v2");
     }
 
     @Override
     public Object run() throws ZuulException {
-        // TODO Auto-generated method stub
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        String path = request.getServletPath();
+
+        /**
+         * Zuul returns and HTTP 200 if there is no route...
+         * So I have to manually return 404
+         * 
+         * https://github.com/spring-cloud/spring-cloud-netflix/issues/1532
+         */
+        Route r = routeLocator.getMatchingRoute(path);
+        if (r == null) {
+            // blocks the request
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(HttpStatus.NOT_FOUND.value());
+        }
+
         return null;
     }
 
     @Override
     public String filterType() {
-        // TODO Auto-generated method stub
         return "pre";
     }
 
     @Override
     public int filterOrder() {
-        // TODO Auto-generated method stub
         return 1;
     }
 }
